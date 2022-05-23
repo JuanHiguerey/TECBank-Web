422611a91f33bcbcb35c62d37f66880cca2a0e3e
JuanHiguerey <72536262+JuanHiguerey@users.noreply.github.com>
2022-05-22 21:21:31 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Login } from './component/Login';
+
+// React 18 Fundamentals Crash Course 2022
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-header'/>
+      <div className='App-body'>
+        <Login/>
+      </div>
     </div>
   );
 }
---
+++ src/component/Login.js
@@ -0,0 +1,38 @@
+import logo from "../res/logo-tecbank.png"
+import { useState } from "react"
+
+export const Login = (props) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault()
+        alert(`The username is ${username} and the password is ${password}`)
+    }
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div>
+                <img src={logo} className='Login-logo' alt="logo"/><br/><br/>
+                <label>Usuario</label><br/>
+                <input 
+                    type='text'
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                /><br/><br/>
+                <label>Contraseña</label><br/>
+                <input 
+                    type='password'
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                /><br/><br/><br/>
+                <button type="submit">Ingresar</button><br/><br/> {/* Este boton tiene un evento onClick donde se mandan las credenciales al servidor */}
+                <hr class="solid"></hr><br/>
+                <button type='button'>Registrarse</button><br/><br/> {/* Este boton tiene un evento onClick donde se instancia otro componente que contiene el form para registrar usuarios */}
+            </div>
+        </form>
+    )
+
+}
+
+//export default Greet
